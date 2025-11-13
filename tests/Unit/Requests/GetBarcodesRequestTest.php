e9a76da8e1235ee584c12f36ac0246bfc8913302
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2025-11-14 01:05:05 +0400

    updated GetBarcodesRequestTest

--- tests/Unit/Requests/GetBarcodesRequestTest.php
+++ tests/Unit/Requests/GetBarcodesRequestTest.php
@@ -7,9 +7,12 @@ use Mchekhashvili\RsWaybill\Dtos\Static\BarcodeDto;
 
 test("returned response is an array of " . BarcodeDto::class, function () {
     $response = (new WaybillServiceConnector())
-        ->send(new GetBarcodesRequest([
-            'bar_code' => 0,
-        ]));
+        ->send(new GetBarcodesRequest(array_merge(
+            [
+                'bar_code' => 0,
+            ],
+            getServiceUserCredentials()
+        )));
     $dto = $response->dto();
     expect($dto)->toBeInstanceOf(ArrayDto::class);
     expect($dto)->toHaveProperty("data");
