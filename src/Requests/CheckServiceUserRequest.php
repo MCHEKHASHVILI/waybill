4dd1735bf0207c1682aca81a92c61cc1fcfbd32a
MCHEKHASHVILI <50997011+MCHEKHASHVILI@users.noreply.github.com>
2026-03-19 23:28:05 +0400

    fix: add null guards to CheckServiceUserRequest DTO construction

--- src/Requests/CheckServiceUserRequest.php
+++ src/Requests/CheckServiceUserRequest.php
@@ -6,7 +6,6 @@ namespace Mchekhashvili\RsWaybill\Requests;
 
 use Saloon\Http\Response;
 use Mchekhashvili\RsWaybill\Enums\Action;
-use Mchekhashvili\RsWaybill\Requests\BaseRequest;
 use Mchekhashvili\RsWaybill\Traits\Requests\HasParams;
 use Mchekhashvili\RsWaybill\Dtos\Static\CheckServiceUserDto;
 use Mchekhashvili\RsWaybill\Interfaces\Requests\HasParamsInterface;
@@ -14,8 +13,11 @@ use Mchekhashvili\RsWaybill\Interfaces\Requests\HasParamsInterface;
 class CheckServiceUserRequest extends BaseRequest implements HasParamsInterface
 {
     use HasParams;
+
     protected Action $action = Action::CHECK_SERVICE_USER;
+
     public function __construct(protected mixed $params = []) {}
+
     public function createDtoFromResponse(Response $response): CheckServiceUserDto
     {
         $data = array_map(
@@ -23,10 +25,12 @@ class CheckServiceUserRequest extends BaseRequest implements HasParamsInterface
             $response->xmlReader()->element("{$this->action->value}Response")->sole()->getContent()
         );
 
+        $resultKey = "{$this->action->value}Result";
+
         return new CheckServiceUserDto(
-            active: strtolower((string) $data["{$this->action->value}Result"]) === "true",
-            tenant_id: (int) $data['un_id'],
-            user_id: (int) $data['s_user_id'],
+            active:    strtolower((string) ($data[$resultKey] ?? '')) === 'true',
+            tenant_id: (int) ($data['un_id']     ?? 0),
+            user_id:   (int) ($data['s_user_id'] ?? 0),
         );
     }
 }
